c33525e30b9178fd29d262f74c737e0dca2c3d23
Dmitri Mendis <2dmendis@gmail.com>
2018-05-16 23:12:13 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,6 +6,14 @@ class School
   end 
   
   def add_student(name, grade)
-    @roster[grade]
+    @roster.each do |grade_num, student|
+      if grade_num == grade 
+        @roster[grade] << name
+      else 
+        @roster[grade] = []
+        @roster[grade] << name 
+      end 
+    #@roster[grade] = []
+    
   end 
 end 
